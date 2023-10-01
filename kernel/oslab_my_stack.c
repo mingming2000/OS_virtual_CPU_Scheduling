c9b6e8458c0be9750b831514c4915c2c148a029f
mingming2000 <alsrud1228@korea.ac.kr>
2023-10-02 02:13:52 +0900

    add header files

--- kernel/oslab_my_stack.c
+++ kernel/oslab_my_stack.c
@@ -1,37 +1,47 @@
-#define MAX = 300
+#include "linux/syscalls.h"
+#include "linux/kernel.h"
+#include "linux/linkage.h"
+
+#define MAX 300
 static int arr[MAX] = {0};
 static int top = -1;
 
 SYSCALL_DEFINE1(os2023_push, int, a){
+    int i;
 
     if(top == MAX-1){
-        printf("ERROR: STACK OVERFLOW");
+        printk("ERROR: STACK OVERFLOW");
     }
     else{
         top++;
         arr[top] = a;
-        printf("Push : %3d\n", a);
 
         printk("Stack Top --------------------\n");
-        for(int i = 0; i <= top; ++i){
+        for(i = 0; i <= top; ++i){
             printk("%3d\n", arr[i]);
         }
         printk("Stack Bottom -----------------\n");
     }
+
+    return a;
 }
 
-SYSCALL_DEFINE2(os2023_pop){
+SYSCALL_DEFINE0(os2023_pop){
+    int push, i;
+
     if(top == -1){
-        printf("ERROR: STACK UNDERFLOW");
+        printk("ERROR: STACK UNDERFLOW");
     }
     else{
-        printf("Pop : %3d", arr[top]);
+        push = arr[top];
         top--;
 
         printk("Stack Top --------------------\n");
-        for(int i = 0; i <= top; ++i){
+        for(i = 0; i <= top; ++i){
             printk("%3d\n", arr[i]);
         }
         printk("Stack Bottom -----------------\n");
     }
+
+    return push;
 }
