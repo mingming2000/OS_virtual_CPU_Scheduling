5ee71dcb4ef182511f21cab15d2f579f6f3dd378
mingming2000 <alsrud1228@korea.ac.kr>
2023-12-05 01:29:07 +0900

    Add RR

--- process_usr.c
+++ process_usr.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MY_CPU 337  // ku_cpu(virtual CPU)  : #337 in the syscall_64.tbl
+#define MY_CPU_FCFS 337  // ku_cpu(FCFS): #337 in the syscall_64.tbl
+#define MY_CPU_SJF 338  // ku_cpu(SJF)  : #338 in the syscall_64.tbl
+#define MY_CPU_SRTF 339 // ku_cpu(SRTF) : #339 in the syscall_64.tbl
+#define MY_CPU_RR 340   // Ku_cpu(RR)   : #340 in the syscall_64.tbl
 
 int main(int argc, char **argv){
     int job;
@@ -25,19 +28,25 @@ int main(int argc, char **argv){
     
     printf("\nProcess: %s | Job time: %d(s)\n", name, job);
     job *= 10;                      // Execute syscall in every 0.1(s)
-
     // Execution
     while(job){
         // Accepted request
-        if(!syscall(MY_CPU, name, job)) job--;
+        // if(!syscall(MY_CPU_FCFS, name, job)){            // FCFS
+        // if(!syscall(MY_CPU_SJF, name, job)){             // SJF
+        if(!syscall(MY_CPU_SRTF, name, job)){               // SRTF
+        // if(!syscall(MY_CPU_RR, name, job)){              // RR
+
+            job--;
+        }
         // Rejected request
         else{
             wait++;
         }
+
         usleep(100000);             // Delay 0.1 second
     }
 
-    syscall(MY_CPU, name, 0);
+    syscall(MY_CPU_RR, name, 0);       // Finish Process
     printf("\n[Finish] Process: %s | Total wait time %d\n", name, (wait+5)/10);
     return 0;
 }
